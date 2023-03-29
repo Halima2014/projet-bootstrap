dd4b225694515e3abb835ed712572b14ef2e8fb7
Halima2014 <benkhalifahalima@hotmail.com>
2023-03-29 16:35:19 +0200

    ajout des icones et des élément

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,13 +4,19 @@ const categorie =`<div class="categorie">
                     <a>Ma categorie</a>
                 </div>`;
 
-const article = `<article class="col-12 col-md-6 col-xl-4">
-                    <div>
-                        <img src="https://picsum.photos/300/150"/>
+const article = `<article class="col-12 col-md-6 col-xl-4 p-2">
+                   
+                    <div class="card shadow" >
+                    <img src="https://picsum.photos/300/150" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Microphone Rode NT1-A</h5>
+                        <p class="card-text">Avec sa capsule de condensateur de 1 pouce et sa réponse en fréquence étendue ...</p>
+                        <a href="#" class="btn btn-primary"  data-bs-toggle="tooltip" data-bs-placement="bottom" title="325 utillisateurs regardent cet article">
+                        <i class="bi bi-cart"></i>  Acheter</a>
                     </div>
-                    <h2>Microphone Rode NT1-A</h2>
-                    <p>Avec sa capsule de condensateur de 1 pouce et sa réponse en fréquence étendue ...</p>
-                </article>`;
+                </article>
+                
+                </div>`;
 
 
 let htmlCategories = '';
@@ -24,3 +30,9 @@ for(let i =0; i<12; i++){
     htmlArticles += article;
 }
 listArticles.innerHTML = htmlArticles;
+
+// Initialisation des toolitps
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
